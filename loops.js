6f66238ef4d21d4f5c5d56dc885c407c7ab1bb2f
Jesse Billard <jessebillard@mac.com>
2018-01-31 14:59:26 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -3,7 +3,7 @@ var makeArray = []
 function forLoop(array) {
  for (let i = 0; i < 25; i++) {
    if (i === 1) {
-   makeArray.push(`I am 1 strange loop.`) 
+   makeArray.push('I am 1 strange loop.') 
    }
    else {
      makeArray.push(`I am ${i} strange loops.`)
